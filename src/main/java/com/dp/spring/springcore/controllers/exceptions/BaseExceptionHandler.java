edcc17d9ef9d138218dd0eb3dfa2f3e5a55ffdc3
Daniele Piano <dpiano01@gmail.com>
2022-02-21 17:00:57 +0100

    fix import

--- src/main/java/com/dp/spring/springcore/controllers/exceptions/BaseExceptionHandler.java
+++ src/main/java/com/dp/spring/springcore/controllers/exceptions/BaseExceptionHandler.java
@@ -8,13 +8,13 @@ import com.dp.spring.springcore.controllers.response.body.FailBody;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
